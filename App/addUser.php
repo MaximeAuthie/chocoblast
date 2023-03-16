ede3422ba4aeafd30361131e3de4d6d86399af01
Maxime Authié <authie.maxime@orange.fr>
2023-03-16 17:51:40 +0100

    formulaire d'inscription

--- App/addUser.php
+++ App/addUser.php
@@ -1,3 +1,93 @@
 <?php
-    //logique PHP
+    //import BDD
+    include '../App/utils/connectBdd.php';
+
+    //Fonction permettant de récupérer l'extension d'une image
+    function get_file_extension($file) {
+        return substr(strrchr($file,'.'),1);
+    }
+
+    //Vérification du formulaire, du format de l'image, de la taille de l'image, de l'existance du compte et exécution de la fonction 'addUser'
+    if (isset($_POST['submit_register'])) {
+        if (!empty($_POST['nom_utilisateur']) AND !empty($_POST['prenom_utilisateur']) AND !empty($_POST['mail_utilisateur']) AND !empty($_POST['password_utilisateur'])) {
+            if ($_FILES['image_utilisateur']['tmp_name']) {
+                $ext= get_file_extension($_FILES['image_utilisateur']['name']);
+                if ($ext == 'jpg' OR $ext == 'jpeg' OR $ext == 'png' or $ext == 'JPG' OR $ext == 'JPEG' OR $ext == 'PNG') {
+                    if ($_FILES['image_utilisateur']['size'] <= (100 * 1024)) {
+                        move_uploaded_file($_FILES['image_utilisateur']['tmp_name'], '../Public/asset/image/'.$_FILES['image_utilisateur']['name'].'');
+                        $imageLink='../Public/asset/image/'.$_FILES['image_utilisateur']['name'];
+                    } else {
+                        $error=2; //La taille de l'image est trop grande
+                        echo 'Image trop grande';
+                    }
+                } else {
+                    $error=3; // Le format de l'image n'est pas bon
+                    echo 'Mauvais format';
+                }
+            } else {
+                $imageLink='../Public/asset/image/default.png';
+            }
+            // Vérification de l'existance du compte et ajout de l'utilisateur si l'adresse mail n'est pas déjà utilisée
+            if (checkAccount($bdd,$_POST['mail_utilisateur'])) {
+                $error=1; // L'adresse mail est déjà utilisée pour un autre compte utilisateur
+                echo 'Utilisateur créé';
+            } else {
+                addUser($bdd,$_POST['nom_utilisateur'], $_POST['prenom_utilisateur'], $_POST['mail_utilisateur'], $_POST['password_utilisateur'], $imageLink);
+                $success='ok';
+            }
+        } else {
+            $error=4; //Tous les champs du formulaire ne sont pas remplis
+            echo 'Tous les champs ne sont pas remplis';
+        }
+    }
+
+    //Gestion des messages
+    if ($success != '') {
+        header('Location:./vue/view_add_user.php?success='.$success.'');
+    }
+    if ($error != '') {
+        header('Location:./vue/view_add_user.php?error='.$error.'');
+    }
+
+    function addUser($bdd, $userLastName, $userFirstName, $userMail, $userPassword, $userImage) {
+        try {
+            $status = 1;
+            $role= 1;
+            //préparation de la requête
+            $reqUpdate = $bdd->prepare('INSERT INTO utilisateur(nom_utilisateur, prenom_utilisateur, mail_utilisateur, password_utilisateur, image_utilisateur, statut_utilisateur, id_roles) VALUES
+            (?, ?, ?, ?, ?, ?, ?)');
+
+            //affection des variables
+            $reqUpdate->bindParam(1, $userLastName, PDO::PARAM_STR); 
+            $reqUpdate->bindParam(2, $userFirstName, PDO::PARAM_STR);
+            $reqUpdate->bindParam(3, $userMail, PDO::PARAM_STR);
+            $reqUpdate->bindParam(4, $userPassword, PDO::PARAM_STR);
+            $reqUpdate->bindParam(5, $userImage, PDO::PARAM_STR);
+            $reqUpdate->bindParam(6, $status, PDO::PARAM_STR);
+            $reqUpdate->bindParam(7, $role, PDO::PARAM_INT);
+
+            //execution de la requête
+            $reqUpdate->execute();
+        }
+        catch(Exception $e){
+            die('Error: '.$e->getMessage());
+        }
+    }
+
+    function checkAccount($bdd, $userMail) {
+        try{
+            //préparation de la requête
+            $reqExist = $bdd->prepare('SELECT mail_utilisateur FROM utilisateur WHERE mail_utilisateur=?');
+
+            //Affection des variables
+            $reqExist->bindParam(1, $userMail, PDO::PARAM_STR);
+
+            //Execution de la requête
+            $reqExist->execute();
+            $result = $reqExist->fetchAll(PDO::FETCH_ASSOC); //permet de stocker les retours de la requête dans un tableau associatif
+            return $result; //retournera la variable quand la fonctionsera exécutée 
+        } catch (Exception $e) {
+            die('Error: '.$e->getMessage());
+        }
+    }
 ?>
--- App/vue/view_add_user.php
+++ App/vue/view_add_user.php
@@ -5,10 +5,56 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../Public/asset/style/main.css">
-    <script src="../../Public/asset/script/script.js" defer></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Inscription</title>
 </head>
 <body>
-    
+    <h1 class="text-center .w-50 mt-5 mb-5">Inscription</h1>
+    <form action="..\addUser.php" method="post" enctype="multipart/form-data" class="w-50 mx-auto">
+        <div class="mb-3">
+            <label for="nom_utilisateur" class="form-label">Nom :</label>
+            <input type="text" class="form-control" id="nom_utilisateur" name="nom_utilisateur">
+        </div>
+        <div class="mb-3">
+            <label for="prenom_utilisateur" class="form-label">Prénom :</label>
+            <input type="text" class="form-control" id="prenom_utilisateur" name="prenom_utilisateur">
+        </div>
+        <div class="mb-3">
+            <label for="mail_utilisateur" class="form-label">Adresse mail :</label>
+            <input type="email" class="form-control" id="mail_utilisateur" name="mail_utilisateur">
+        </div>
+        <div class="mb-3">
+            <label for="password_utilisateur" class="form-label">Mot de passe :</label>
+            <input type="password" class="form-control" id="password_utilisateur" name="password_utilisateur">
+        </div>
+        <div class="mb-3">
+            <label for="image_utilisateur" class="form-label">Image de profil :</label>
+            <input type="file" class="form-control" id="image_utilisateur" name="image_utilisateur">
+        </div>
+        <div class="text-center mt-5 mb-5"><button type="submit" class="btn btn-primary" name="submit_register">S'inscrire</button></div>
+        
+    </form>
+    <script src="../../Public/asset/script/script.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
+
+<?php
+    if (isset($_GET['success'])) {
+        echo '<div class="alert alert-success w-50 mx-auto mt-3 text-center" role="alert">Votre compte a bien été créé</div>';
+    }
+    if (isset($_GET['error'])) {
+        if ($_GET['error'] == 1) {
+            echo '<div class="alert alert-danger w-50 mx-auto text-center" role="alert">L\'adresse mail saisie est déjà utilisée par un autre compte</div>';
+        }
+        if ($_GET['error'] == 2) {
+            echo '<div class="alert alert-danger w-50 mx-auto text-center" role="alert">La taille de l\'image ne doit pas dépassée 100ko.</div>';
+        }
+        if ($_GET['error'] == 3) {
+            echo '<div class="alert alert-danger w-50 mx-auto text-center" role="alert">Le format de l\'image n\'est pas correct. Les formats acceptées sont .jpg, .jpeg et .png.</div>';
+        }
+        if ($_GET['error'] == 4) {
+            echo '<div class="alert alert-danger w-50 mx-auto text-center" role="alert">Veuillez compléter tous les champs du formulaire.</div>';
+        }
+    }
+?>
