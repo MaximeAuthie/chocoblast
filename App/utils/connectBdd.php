f29640611db77f1cf683aeb2f5e46d97b4040a4e
Maxime Authié <authie.maxime@orange.fr>
2023-04-05 18:34:34 +0200

    Ajout fichier env

--- App/utils/connectBdd.php
+++ App/utils/connectBdd.php
@@ -1,9 +1,14 @@
 <?php
     //Classe contenant la méthode statique permettant de se connecter à la BDD
     class BddConnect {
+        
         static function connexion () {
-            return new PDO('mysql:host=localhost;dbname=chocoblast', 'root','', 
-            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            //Import du ficher de configuration pour récupérer les variables de connexion
+            include './env.php';
+            
+            //Nouvel objet PDO
+            return new PDO("mysql:host=$host;dbname=$database", $login, $password, 
+            array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
         }
     }
 
